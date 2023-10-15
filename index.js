5229c10d53d03ba476252f5de5bcbbfa8e8f59ec
Hugodz <hugosilva1ads@gmail.com>
2023-10-15 11:34:06 -0300

    S

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let nomeDoHeroi = "Higão"
 let Experiencia = ""
 
 do {
-  Experiencia = prompt(5);
+  Experiencia = prompt("dIGITE UM NUMERO VALIDO");
 } while(Experiencia < 1 || Experiencia > 10);
 
 console.log(nomeDoHeroi + `Você digitou o número ${Experiencia}.`);
