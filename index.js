3c8e471990e0f6614353e7919d5cbf542a74caf7
Hugodz <hugosilva1ads@gmail.com>
2023-10-15 11:35:41 -0300

    s

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-let nomeDoHeroi = "Higão"
-let Experiencia = ""
+let dia = "terça"; // Você pode mudar isso para outros dias
 
-do {
-  Experiencia = prompt("dIGITE UM NUMERO VALIDO");
-} while(Experiencia < 1 || Experiencia > 10);
-
-console.log(nomeDoHeroi + `Você digitou o número ${Experiencia}.`);
+switch (dia) {
+  case "segunda":
+    console.log("Dia de começar a semana com energia!");
+    break;
+  case "terça":
+    console.log("Hora de aprender coisas novas!");
+    break;
+  case "quarta":
+    console.log("Metade da semana, continue firme!");
+    break;
+  case "quinta":
+    console.log("Quase lá, um pouco mais de esforço!");
+    break;
+  case "sexta":
+    console.log("Dia de comemorar, o fim de semana está chegando!");
+    break;
+  default:
+    console.log("Final de semana! Descanse e divirta-se!");
+}
