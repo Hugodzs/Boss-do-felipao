1c88bd6e1365951222e1f2f3058688a5e8de0007
Hugodz <hugosilva1ads@gmail.com>
2023-10-15 11:27:07 -0300

    w

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 let nomeDoHeroi = "Higão"
-let Experiencia = 500
+let Experiencia = 5
 
-switch (Experiencia) {
-    case <=1000:
-         console.log(nomeDoHeroi +  "  está no nivel Ferro" );
-    break;
+do {
+  Experiencia = prompt('Digite um número entre 1 e 10:');
+} while(Experiencia < 1 || Experiencia > 10);
 
-}
+console.log(nomeDoHeroi + `Você digitou o número ${numero}.`);
