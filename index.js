067a8cd32a2857d022a9c8a2bb39dd8d2ec4c992
Hugodz <hugosilva1ads@gmail.com>
2023-10-15 11:51:42 -0300

    s

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //if,else if ,else
-let nivelDeFome =  500
+let nivelDeFome =  100
 
-if(nivelDeFome <= 500){
+if(nivelDeFome < 1001){
 console.log("voce está no nivel barro")
-}else if(nivelDeFome ===2){
+}else if(nivelDeFome >=1001){
 console.log("muita fome")
 }else{
 console.log("Você comeria mais que o pica pau")
