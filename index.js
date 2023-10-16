ee089984eebd238efada60fb0ba8fd4e31ffe606
Hugo Silva <145476600+Hugodzs@users.noreply.github.com>
2023-10-16 12:45:08 +0000

    go

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ switch (ranking) {
         ranking = "Radiante"
         break;      
   default:
-    console.log("Digite um valor válido de Experiencia");
+    ranking = "Invalido";
 }
-console.log(nomeHero + "tem" + experiencia + 'pontos de Experiência e está no ranking'+ ranking + "!")
+console.log(nomeHero + " tem " + experiencia + ' pontos de Experiência e está no ranking '+ ranking + "!")
