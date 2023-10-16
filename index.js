19285debfa04b5e29a155f7d982efe97cffeec87
Hugo Silva <145476600+Hugodzs@users.noreply.github.com>
2023-10-16 12:29:30 +0000

    atual

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-//if,else if ,else
-let nivelDeFome =  2000
 
-if(nivelDeFome < 1001){
-console.log("voce está no nivel barro")
-}else if(nivelDeFome >=1001){
-console.log("muita fome")
-}else{
-console.log("Você comeria mais que o pica pau")
+let nomeHero = "Hugow"
+
+let ranking
+
+let experiencia = 1200
+
+switch (ranking) {
+  case (experiencia<=1000) :
+    ranking = "Ferro"
+    break;
+  case (experiencia>=1001 && experiencia<=2000) :
+        ranking = "Bronze"
+        break;
+  case (experiencia>=2001 && experiencia<=5000) :
+        ranking = "Prata"
+        break;
+  case (experiencia>=5001 && experiencia<=7000) :
+        ranking = "Ouro"
+        break;
+  case (experiencia<=1001) :
+        ranking = "Platina"
+        break;
+  case (experiencia<=1001) :
+        ranking = "Ascendente"
+        break;  
+  case (experiencia<=1001) :
+        ranking = "Imortal"
+        break; 
+  case (experiencia<=1001) :
+        ranking = "Radiante"
+        break;      
+  default:
+    console.log("Final de semana! Descanse e divirta-se!");
 }
