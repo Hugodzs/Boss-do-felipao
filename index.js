21e83ac0298d7a3838cc301e462dba21f44b5bee
Hugodz <hugosilva1ads@gmail.com>
2023-10-17 12:19:41 -0300

    s

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 //variaveis
 let nameHero = "Hugo"
 
-let experiencia = "1100"
+let experiencia = "10002"
 
-if(experiencia <= "1200" ){
-    console.log(nameHero + "Você esta ranking Ferro")
+if(experiencia <= "1000" ){
+    console.log(nameHero + " Tem "+ experiencia + " pontos de Experiência e está no ranking Ferro")
 }
-else if(experiencia >"1200" && experiencia <="2000"){
-    console.log(nameHero + "Está no ranking Prata")     
+else if(experiencia >="1001" && experiencia <="2000"){
+    console.log(nameHero + " Tem "+ experiencia + " pontos de Experiência e está no ranking Bronze")
 }
+else if(experiencia >="2001" && experiencia <="5000"){
+    console.log(nameHero + " Tem "+ experiencia + " pontos de Experiência e está no ranking Prata")
+}
+else if(experiencia >="5001" && experiencia <="7000"){
+    console.log(nameHero + " Tem "+ experiencia + " pontos de Experiência e está no ranking Ouro")
+}
+else if(experiencia >="7001" && experiencia <="8000"){
+    console.log(nameHero + " Tem "+ experiencia + " pontos de Experiência e está no ranking Platina")
+}
+else if(experiencia >="8001" && experiencia <="9000"){
+    console.log(nameHero + " Tem "+ experiencia + " pontos de Experiência e está no ranking Ascendente")
+}
+else if(experiencia >="9001" && experiencia <="10000"){
+    console.log(nameHero + " Tem "+ experiencia + " pontos de Experiência e está no ranking Imortal")
+}
+else if(experiencia >="10001"){
+    console.log(nameHero + " Tem "+ experiencia + " pontos de Experiência e está no ranking Radiante")
+}
+
 else{console.log("Digite um valor válido")
 }
